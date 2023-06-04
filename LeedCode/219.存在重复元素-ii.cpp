c0f2517a8c94072ceb12d7ec1c63b01c3a9ee1e0
kk <921232958@qq.com>
2023-06-05 01:56:53 +0800

    LeedCode 0605

--- LeedCode/219.存在重复元素-ii.cpp
+++ LeedCode/219.存在重复元素-ii.cpp
@@ -1,3 +1,11 @@
+// @before-stub-for-debug-begin
+#include "commoncppproblem219.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @lc app=leetcode.cn id=219 lang=cpp
  *
@@ -5,6 +13,7 @@
  */
 
 #include <map>
+#include <queue>
 #include <vector>
 
 using namespace std;
@@ -14,6 +23,11 @@ class Solution
 {
 public:
     bool containsNearbyDuplicate(vector<int> &nums, int k)
+    {
+        return ans1(nums, k);
+    }
+
+    bool ans1(vector<int> &nums, int k)
     {
         map<int, int> mapnum;
         for (int i = 0; i < nums.size(); i++)
@@ -26,6 +40,42 @@ public:
                     return true;
                 else
                     mapnum[nums[i]] = i;
+            }
+        }
+        return false;
+    }
+
+    bool ans2(vector<int> &nums, int k)
+    {
+        if (k == 0)
+            return false;
+        deque<int> dequenum;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (dequenum.empty())
+                dequenum.push_back(nums[i]);
+            else
+            {
+                if (dequenum.size() < k)
+                {
+                    for (int j = 0; j < dequenum.size(); j++)
+                    {
+                        if (dequenum[j] == nums[i])
+                            return true;
+                    }
+                    dequenum.push_back(nums[i]);
+                }
+                else
+                {
+                    for (int j = 0; j < dequenum.size(); j++)
+                    {
+                        if (dequenum[j] == nums[i])
+                            return true;
+                    }
+                    dequenum.pop_front();
+                    dequenum.push_back(nums[i]);
+                }
             }
         }
         return false;
