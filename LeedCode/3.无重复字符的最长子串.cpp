a358e74114f4d7171f60e7a123a5266eaf0151a6
kk <921232958@qq.com>
2023-08-29 17:02:41 +0800

    20230829

--- LeedCode/3.无重复字符的最长子串.cpp
+++ LeedCode/3.无重复字符的最长子串.cpp
@@ -4,12 +4,67 @@
  * [3] 无重复字符的最长子串
  */
 
-// @lc code=start
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
+#include <bits/stdc++.h>
 
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+#if 0
+    int lengthOfLongestSubstring(string s)
+    {
+        return process(s, 0);
     }
+
+    int process(string s, int index)
+    {
+        if (index == s.length())
+            return 0;
+        int nocur = process(s, index + 1);
+        set<char> setch;
+        setch.insert(s[index]);
+        int cur = 1;
+        for (int i = index + 1; i < s.length(); i++)
+        {
+            if (setch.find(s[i]) == setch.end())
+            {
+                cur++;
+                setch.insert(s[i]);
+            }
+            else
+                break;
+        }
+        return max(cur, nocur);
+    }
+#else
+    int lengthOfLongestSubstring(string s)
+    {
+        int res = INT_MIN;
+        int left = 0;
+        int right = 0;
+        map<char, int> setmap;
+        int cur = 0;
+        while (right < s.length())
+        {
+            auto iter = setmap.find(s[right]);
+            if (iter == setmap.end())
+            {
+                setmap.insert(make_pair(s[right], right));
+                right++;
+            }
+            else
+            {
+                res = max(res, right - left);
+                int repeat = setmap[s[right]];
+                for (int i = left; i < repeat + 1; i++)
+                    setmap.erase(s[i]);
+                left = repeat + 1;
+            }
+        }
+        return max(res, right - left);
+    }
+#endif
 };
 // @lc code=end
-
---
+++ LeedCode/5.最长回文子串.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=cpp
+ *
+ * [5] 最长回文子串
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    string longestPalindrome(string s)
+    {
+    }
+};
+// @lc code=end
